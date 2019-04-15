0bb2ce7d44550a6e8e035e6dd06a5ba786e394fa
Derek J Leong <derekjleong@gmail.com>
2019-04-15 14:18:42 -0700

    Add files via upload

---
+++ headerBackground.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./headerBackground.css";
+
+class HeaderBackground extends Component {
+   state = {};
+   render() {
+      return (
+         <div className="container">
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+            <div className="shootingStar" />
+         </div>
+      );
+   }
+}
+
+export default HeaderBackground;
